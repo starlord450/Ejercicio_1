4bf63c8f2efba42cc5f93b23e3bc3a848696b315
StarLord450 <jorgeschneiderdiaz@gmail.com>
2023-12-10 19:19:05 +0100

    multiply -divide

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -25,6 +25,26 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "3", "ERROR ADD"
         )
 
+    def test_api_add(self):
+        url = f"{BASE_URL}/calc/multiply/1/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "2", "ERROR MULTIPLY"
+        )
+
+    def test_api_add(self):
+        url = f"{BASE_URL}/calc/divide/1/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "0", "HTTP 406"
+        )
+
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
