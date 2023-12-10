2d1bc838fad44e34606031c1c290375498bccbb4
StarLord450 <jorgeschneiderdiaz@gmail.com>
2023-12-11 00:19:21 +0100

    divide

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -45,15 +45,7 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "2", "ERROR MULTIPLY"
         )
 
-    def test_api_divide(self):
-        url = f"{BASE_URL}/calc/divide/3/2"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-        self.assertEqual(
-            response.read().decode(), "1", "ERROR 406"
-        )
+
 
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
