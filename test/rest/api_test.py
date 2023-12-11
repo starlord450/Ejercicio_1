5da91e11f291125f175935d4894b135384a5a644
StarLord450 <jorgeschneiderdiaz@gmail.com>
2023-12-11 17:50:40 +0100

    divide

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -25,7 +25,15 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "3", "ERROR ADD"
         )
     
-
+    def test_api_substract(self):
+        url = f"{BASE_URL}/calc/substract/1/0"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "1", "ERROR substract"
+        )
 
     def test_api_multiply(self):
         url = f"{BASE_URL}/calc/multiply/1/2"
@@ -38,13 +46,13 @@ class TestApi(unittest.TestCase):
         )
 
     def test_api_divide(self):
-        url = f"{BASE_URL}/calc/divide/4/1"
+        url = f"{BASE_URL}/calc/divide/2/2"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
         self.assertEqual(
             response.status, http.client.OK, f"Error en la petición API a {url}"
         )
         self.assertEqual(
-            response.read().decode(), "2", "ERROR 406"
+            response.read().decode(), "0", "ERROR 406"
         )
 
     def test_api_sqrt(self):
